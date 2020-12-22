d134ced3841688e2a7ce5f6ace96c40c78e965b2
johnjamespj <johnjamespj@gmail.com>
2020-12-22 11:29:50 -0500

    added instructions

--- src/components/DisplayActionPanel.jsx
+++ src/components/DisplayActionPanel.jsx
@@ -6,6 +6,8 @@ export function DisplayActionPanel({ moves, time, mineCount, flags, paused, onTo
       <span>Time : {Math.floor(time / 1000)}s / </span>
       <span>Moves : {moves} / </span>
       <span>Flags: {mineCount - flags} </span>
+      <span>(Right click to flag)</span><br/>
+      <span>Minesweeper BY john</span>
       <div>
         <button onClick={onTogglePause}>{paused ? "start" : "pause"}</button>
         <button onClick={onNewGame}>New Game</button>
